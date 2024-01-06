ee3e157e9f09cf11c629a4d3581927e43db0458e
MehdiA7 <godefroid.mehdi@gmail.com>
2024-01-06 11:47:32 +0100

    Initial commit

---
+++ src/main/java/com/mycompagny/helloworld/HelloUniverse.java
@@ -0,0 +1,12 @@
+package com.mycompagny.helloworld;
+
+public class HelloUniverse {
+    public static void main(String... args){
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de :");
+        int nbPlanete=8;
+        System.out.println(nbPlanete);
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de :");
+        nbPlanete=9;
+        System.out.println(nbPlanete);
+    }
+}
